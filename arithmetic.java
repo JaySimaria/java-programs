2042575ccf3dcce1f6d5e6491ce85b8829c8c51d
JaySimaria <jaysimaria1808@gmail.com>
2021-02-16 05:58:39 +0530

    new program

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+class arithmetic
+{
+	public static void main(String args[])
+	{
+			int a=10,b=5;
+			System.out.println("A="+a);
+			System.out.println("B="+b);
+			System.out.println("A+b="+(a+b));
+			System.out.println("A-b="+(a-b));
+			System.out.println("A*b="+(a*b));
+			System.out.println("A/b="+(a/b));
+			System.out.println("A%b="+(a%b));
+	}
+}	
+	
+	
