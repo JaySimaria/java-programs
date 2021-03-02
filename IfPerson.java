9af72dfc2a19d387046f0a40f15d639eb86aaff1
JaySimaria <jaysimaria1808@gmail.com>
2021-03-02 21:03:51 +0530

    java program

---
+++ IfPerson.java
@@ -0,0 +1,30 @@
+class IfPerson
+{
+	public static void main(String args[])
+	{
+		int age=35;
+		char gender='F';
+		if(age>35)
+		{
+			if(gender=='M')
+			{
+				System.out.println("Man");
+			}
+			else
+			{
+				System.out.println("Women");
+			}
+		}
+		else
+		{
+			if(gender=='M')
+			{
+				System.out.println("Boy");
+			}
+			else
+			{
+				System.out.println("Girl");
+			}
+		}
+	}
+}	
